73d302370326143567caf951595d63d94b16708a
Thrasyvoulos Karydis <karydis@mit.edu>
2016-11-17 05:33:27 -0500

    Making aa2hot backwards compatible.

--- evolutron/networks/__init__.py
+++ evolutron/networks/__init__.py
@@ -3,3 +3,16 @@
 from .krs.comet import DeepCoDER
 
 from .krs.extra_layers import Deconvolution1D, Dedense, Unpooling1D
+
+from .krs import extra_layers as extra
+
+custom_layers = {
+    'Convolution1D': extra.Convolution1D,
+    'Deconvolution1D': Deconvolution1D,
+    'MaxPooling1D': extra.MaxPooling1D,
+    'Unpooling1D': Unpooling1D,
+    'Dense': extra.Dense,
+    'Dedense': Dedense,
+    'Reshape': extra.Reshape,
+    'Flatten': extra.Flatten
+}
