da73834835cdbf2d5a08f93eb25172a7108f968a
Thrasyvoulos Karydis <karydis@mit.edu>
2017-05-12 10:41:33 -0400

    load model

--- evolutron/extra_objectives.py
+++ evolutron/extra_objectives.py
@@ -1,12 +1,21 @@
+# coding=utf-8
 import keras.backend as K
+from keras.objectives import mean_squared_error
 
 Beta = 1
 
 
+def masked_mse(inp, decoded, mask_value=0.0):
+    boolean_mask = K.any(K.not_equal(inp, mask_value), axis=-1, keepdims=True)
+    decoded = decoded * K.cast(boolean_mask, K.floatx())
+    return mean_squared_error(y_true=inp, y_pred=decoded)
+
+
 def multiclass_categorical_crossentropy(output, target):
-    """Categorical crossentropy between an output tensor
-    and a target tensor, where the target is a tensor of the same
-    shape as the output. Supports multiclass classification.
+    """
+        Categorical crossentropy between an output tensor
+        and a target tensor, where the target is a tensor of the same
+        shape as the output. Supports multiclass classification.
     """
 
     # manual computation of crossentropy
@@ -16,7 +25,7 @@ def multiclass_categorical_crossentropy(output, target):
 
 
 def pred_pos_approx(y_pred, category):
-    #nb_categories = K.shape(y_pred)[-1]
+    # nb_categories = K.shape(y_pred)[-1]
     nb_categories = 8
 
     pp = 1
@@ -29,10 +38,10 @@ def pred_pos_approx(y_pred, category):
 
 
 def multiclass_loss_precision(y_true, y_pred):
-    """This metric return a smooth approximation of the precision score for each class"""
-    #nb_examples = K.shape(y_true)[0]
-    #nb_categories = K.shape(y_true)[-1]
-    #nb_examples = K.count_params(y_true) / 8
+    """This metric returns a smooth approximation of the precision score for each class"""
+    # nb_examples = K.shape(y_true)[0]
+    # nb_categories = K.shape(y_true)[-1]
+    # nb_examples = K.count_params(y_true) / 8
     nb_categories = 8
     precision_dict = {}
 
@@ -68,7 +77,7 @@ def multiclass_loss_recall(y_true, y_pred):
     """This function return a smooth approximation of the recall score for each class"""
     # nb_examples = K.shape(y_true)[0]
     # nb_categories = K.shape(y_true)[-1]
-    #nb_examples = K.sum(y_true)
+    # nb_examples = K.sum(y_true)
     nb_categories = 8
     recall_dict = {}
 
@@ -99,9 +108,10 @@ def recall_loss(y_true, y_pred):
 
     return sum / nb_categories
 
+
 def fmeasure_loss(y_true, y_pred):
     """This function return a macro-averaged F-score"""
     precision_value = precision_loss(y_true, y_pred)
     recall_value = recall_loss(y_true, y_pred)
 
-    return ((Beta**2 + 1) * precision_value * recall_value) / (Beta**2 * precision_value + recall_value)
+    return ((Beta ** 2 + 1) * precision_value * recall_value) / (Beta ** 2 * precision_value + recall_value)
